f8c0ac3366bc9c4d0307fd24baa43f3cbaefa2f0
urzp <ermak80_pass@mail.ru>
2023-10-21 09:35:30 +0300

    compliter user page

--- src/components/UiUser.vue
+++ src/components/UiUser.vue
@@ -10,7 +10,11 @@
         <div class="title">Изображение</div>
         <label for="files" class="btnGetFile" onclick="document.getElementById('getFile').click()">Загрузить изображение</label>
         <input type="file" id="getFile" style="display:none" name="img" accept="image/png, image/jpeg" />
-        <input type="submit" class="submitButton" value="Отправить" />
+        <RouterLink class="link" to="/">Изменить пароль</RouterLink>
+        <div class="buttons">
+            <div class="canselButton">Отмена</div>
+            <input type="submit" class="submitButton" value="Отправить" />
+        </div>
     </form>
 </template>
 
@@ -72,9 +76,25 @@ form input{
 
 }
 
-.submitButton{
+.link{
     display: block;
     margin-top: 30px;
+    margin-bottom: 40px;
+    text-align: left;
+    font-family: 'Intro-Book';
+    font-size: 16px;  
+    color: #ffffffaf; 
+}
+
+.buttons{
+    display: flex;
+    justify-content: flex-start;
+}
+
+.submitButton, .canselButton{
+    display: block;
+    margin-top: 30px;
+    margin-right: 10px;
     height: 55px;
     width: 190px;
     font-family: 'Intro-Bold-Alt';
@@ -85,5 +105,15 @@ form input{
     border:none;
     cursor: pointer;
 }
+
+.canselButton{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    border: 1px solid #fff;
+}
+
+
 
 </style>
