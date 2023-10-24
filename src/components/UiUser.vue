c23db9cb97095aedb819b3e161bcfbbcb32a9307
urzp <ermak80_pass@mail.ru>
2023-10-24 16:06:58 +0300

    pause user form

--- src/components/UiUser.vue
+++ src/components/UiUser.vue
@@ -2,49 +2,107 @@
     <div class="title">Профиль</div>
     <form action="">
         <div class="title">Название компании</div>
-        <input name="id" type="hidden" :value="user.id">
+
+        <input name="id" type="hidden" :value="id">
         <input name="session" type="hidden" :value="session">
-        <input name="name" placeholder="Альфа банк" :value ="user.name"/>
+
+        <input name="name" placeholder="Альфа банк" v-model="name"/>
+        <div v-if="v$.name.$error" class="error-message" >поле недолжно быть пустым</div>
+        
         <div class="title">Email</div>
-        <input name="email" placeholder="info@alfabank.ru" :value = "user.email"/>
+        <input name="email" placeholder="info@alfabank.ru" v-model="email"/>
+        <div v-if="v$.email.$error" class="error-message">Введите корректный email</div>
+
         <div class="title">Телефон</div>
-        <input name="tel" placeholder="+7 (495) 999 99 99"  :value ="user.telephon" data-phone-pattern/>
+        <input name="tel" placeholder="+7 (495) 999 99 99"  v-model ="tel_number" data-phone-pattern/>
+
         <div class="title">Изображение</div>
         <label for="files" class="btnGetFile" onclick="document.getElementById('getFile').click()">Загрузить изображение</label>
         <input type="file" id="getFile" style="display:none" name="img" accept="image/png, image/jpeg" />
+
         <div class="link" @click="editPassword = !editPassword">{{editPassword?'Не изменять пароль':'Изменить пароль'}}</div>
         <div v-if="editPassword" class="changePassword">
-            <input name="editPassword" type="hidden" :value="editPassword">
+            <input name="editPassword" type="hidden" v-model="editPassword">
+
             <div class="title">Старый пароль</div>
-            <input type="password" name="oldPassword" />
+            <input type="password" name="oldPassword" v-model="oldPassword"/>
+            <div v-if="v$.oldPassword.$error" class="error-message" >поле недолжно быть пустым</div>
+
             <div class="title">Новый пароль</div>
-            <input type="password" name="newPassword" />
+            <input type="password" name="newPassword"  v-model="newPassword"/>
+            <div v-if="v$.newPassword.$error" class="error-message" >поле недолжно быть пустым</div>
+
             <div class="title">Проверить новый пароль</div>
-            <input type="password" name="newPassword" />
+            <input type="password" name="confirmPassword"  v-model="confirmPassword"/>
+            <div v-if="isErrMinLengt(v$.confirmPassword.$errors)" class="error-message" >пароль минимум 6 знаков</div>
+            <div v-if="isErrSame(v$.confirmPassword.$errors)" class="error-message" >пароль несопадает</div>
         </div>
+
         <div class="buttons">
             <div class="canselButton">Отмена</div>
-            <input type="submit" class="submitButton" value="Отправить" />
+            <div class="submitButton" @click="submit()">Отправить</div>
+            <input type="submit"  value="" style="display: none;" />
         </div>
     </form>
 </template>
 
 <script>
 import useVuelidate from '@vuelidate/core'
-import { required, email, minLength } from '@vuelidate/validators'
+import { required, email, minLength, sameAs } from '@vuelidate/validators'
 import telMask from '@/scripts/tel_mask'
 export default{
     name: 'UiUser',
+    setup () {
+        return { v$: useVuelidate() }
+    },
+    created(){
+        let user = JSON.parse(sessionStorage.getItem('user'))
+        this.id = user.id
+        this.name = user.name
+        this.email = user.email
+        this.telephon = user.telephon
+    },
     mounted(){
         telMask()
     },
     data(){
         return{
-            editPassword: false,
             user:JSON.parse(sessionStorage.getItem('user')),
-            session: sessionStorage.getItem('session')
+            id: '',
+            session: sessionStorage.getItem('session'),
+            name: '',
+            email:'',
+            tel_number:'',
+            editPassword: false,
+            oldPassword:'',
+            newPassword:'',
+            confirmPassword:'',
         }
-    }
+    },
+    validations(){
+        return{
+            name: { required },
+            email:{ required, email },
+            oldPassword: { required, minLength:minLength(6) },
+            newPassword: { required, minLength:minLength(6) },
+            confirmPassword: { required, minLength:minLength(6), sameAs: sameAs(this.newPassword) },
+        }
+    },
+    methods:{
+        submit(){
+            this.v$.$validate()
+            window.er = this.v$.confirmPassword.$errors
+        },
+        isErrMinLengt(errors){
+            if(!errors) return false
+            return !!errors.find(item => item.$validator == 'minLength')
+        },
+        isErrSame(errors){
+            if(!errors) return false
+            return !!errors.find(item => item.$validator == 'sameAs')
+        }
+    },
+
 }
 
 </script>
@@ -117,7 +175,9 @@ form input{
 }
 
 .submitButton, .canselButton{
-    display: block;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     margin-top: 40px;
     margin-right: 10px;
     height: 55px;
@@ -132,13 +192,14 @@ form input{
 }
 
 .canselButton{
-    display: flex;
-    align-items: center;
-    justify-content: center;
     background-color: transparent;
     border: 1px solid #fff;
 }
 
 
+.error-message {
+    margin-top: 5px;
+    color: #e7e7e7;
+}
 
 </style>
