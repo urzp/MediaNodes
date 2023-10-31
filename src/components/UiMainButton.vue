0082cb560f1a329acf04c622c5cebc457bfa10c1
urzp <ermak80_pass@mail.ru>
2023-10-31 11:26:50 +0300

    update homepage

---
+++ src/components/UiMainButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="button">
+        <slot/>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'UiBigButton',
+
+}
+
+</script>
+
+<style scoped>
+    .button{
+        width: 350px;
+        height: 210px;
+        margin-right: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-radius: 10px;
+        background-color: #2F2F2F;
+        transition: 0.5s;
+        cursor: pointer;
+    }
+    .button:hover{
+        background-color: #010101;
+        transition: 0.5s
+    }
+    :slotted( img ) {
+        max-width: 70px;
+        max-height: 70px;
+        margin-bottom: 30px;
+    }
+    :slotted( .name ) {
+       color: #fff;
+       font-family: 'Intro-Bold-Alt';
+       font-size: 18px; 
+    }
+    
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,12 @@
   <UiHeader/>
   <UiLeftBar selcted="home"/>
   <div class="content">
-    
+    <div class="title">Добро пожаловать в Личный кабинет!</div>
+    <div class="buttons">
+      <UiMainButton @click="$router.push('/players')"><img src="@/assets/icons/bigButtons/player.svg" alt=""><div class="name">Плееры</div></UiMainButton>
+      <UiMainButton @click="$router.push('/user')"><img src="@/assets/icons/bigButtons/user.svg" alt=""><div class="name">Профиль</div></UiMainButton>
+      <UiMainButton @click="$router.push('/support-user')"><img src="@/assets/icons/bigButtons/support.svg" alt=""><div class="name">Техподдержка</div></UiMainButton>
+    </div>
   </div>
 </template>
 
@@ -10,12 +15,14 @@
 // @ is an alias to /src
 import UiLeftBar from '@/components/UiLeftBar.vue'
 import UiHeader from '@/components/UiHeader.vue'
+import UiMainButton from '@/components/UiMainButton.vue'
 
 export default {
   name: 'HomeView',
   components: {
     UiLeftBar,
     UiHeader,
+    UiMainButton
   }
 }
 </script>
@@ -31,6 +38,28 @@ export default {
     flex-direction: column;
     padding-left: 330px;
     width: calc(100% - 300px - 70px);
+
+    background-image: url('@/assets/icons/backimg.svg');
+    background-position: center;
+    background-repeat: no-repeat;
+    height: calc(100% - 105px);
+  }
+
+  .title{
+    font-family: 'Intro-Bold-Alt';
+    font-size: 20px;
+    color: #fff;
+    margin-top: 20px;
+  }
+
+  .buttons{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    margin-top: 30px;
+    width: 100%;
+    height: 100%;
   }
 
 </style>
